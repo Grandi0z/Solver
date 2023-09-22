eed99e9e660712bc13f26dd55c2b309093e15512
Grandi0z <josephlkhausi@gmail.com>
2023-09-22 22:05:12 +0200

    TDD for the function fizzbizz

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,13 +1,28 @@
 class Solver
   def factorial(numb)
-    raise ArgumentError, 'n must be an integer' unless numb.is_a?(Integer)
-    raise ArgumentError, 'n must be non-negative' if numb.negative?
+    raise ArgumentError, 'numb must be an integer' unless numb.is_a?(Integer)
+    raise ArgumentError, 'numb must be non-negative' if numb.negative?
 
     result = 1
     while numb.positive?
       result *= numb
       numb -= 1
     end
+    result
+  end
+
+  def fizzbuzz(numb)
+    raise ArgumentError, 'must be an integer' unless numb.is_a?(Integer)
+
+    result = ''
+
+    result += 'fizz' if (numb % 3).zero?
+
+    result += 'buzz' if (numb % 5).zero?
+
+    return numb.to_s if result.empty?
+
+
     result
   end
 end
