14b05df2d99efeaf92667437005ab1f96e9a9b1f
Grandi0z <josephlkhausi@gmail.com>
2023-09-22 21:50:09 +0200

    create function factorial

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,3 +1,16 @@
 class Solver
-    
+  def factorial(numb)
+    raise ArgumentError, 'n must be an integer' unless numb.is_a?(Integer)
+    raise ArgumentError, 'n must be non-negative' if numb.negative?
+
+    result = 1
+    while numb.positive?
+      result *= numb
+      numb -= 1
+    end
+    result
+  end
 end
+solver = Solver.new
+
+puts solver.factorial(0)
